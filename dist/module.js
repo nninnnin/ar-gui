6e31b35e2c207381b595af5627c394bfc2d5b4e3
nninnnin <nninnnin7@gmail.com>
2025-01-02 15:18:43 +0900

    Add esModule build

---
+++ dist/module.js
@@ -0,0 +1,8 @@
+import {GUI as $5OpyM$GUI} from "dat.gui";
+
+
+const $cf838c15c8b009ba$var$gui = new $5OpyM$GUI();
+console.log("imported dat gui: ", $cf838c15c8b009ba$var$gui);
+
+
+//# sourceMappingURL=module.js.map
