9ff8a5a4e48812e38b77864a948fb4c9d8acb266
donggyu <watch@rebel9.co.kr>
2025-01-02 15:04:46 +0900

    First parcel build

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+var $gXNCa$datgui = require("dat.gui");
+
+
+const $4fa36e821943b400$var$gui = new $gXNCa$datgui.GUI();
+console.log("imported dat gui: ", $4fa36e821943b400$var$gui);
+
+
+//# sourceMappingURL=main.js.map
